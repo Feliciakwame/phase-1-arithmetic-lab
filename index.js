c69e40aa785b7ae8f85b6af213061e90da446214
Felicia Kwame <kwamefelicia2005@gmail.com>
2024-07-13 03:27:56 +0300

    assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Write your code herelet num1 = 32;
+let num2 = 2;
+let num1 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.ceil(Math.random() * 2) + 1;
+console.log(random);
+
+let num3 = 34;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(5, 3, 12, 20);
+console.log(max);
